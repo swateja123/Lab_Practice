ad3f7fb49f30fcac1000352cd3e454f86faf85ef
Swateja <swateja.bagalab@gmail.com>
2022-02-22 17:28:33 +0530

    commit

--- JavaLab3/src/main/java/Team_Result/GameResult.java
+++ JavaLab3/src/main/java/Team_Result/GameResult.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
+
 //Implementation and main method
 public class GameResult {
     //Read the CSV file
@@ -35,7 +37,7 @@ public class GameResult {
             System.out.println(t2.get(i).getTeamName()+"  |  "+t2.get(i).getPoints());
         }
     }
-    public void createTeams()
+    public void createTeams(ArrayList<FootballTeam> footballList, ArrayList<BasketballTeam> basketballList)
     {
 
         //Read file
@@ -46,8 +48,7 @@ public class GameResult {
             e1.printStackTrace();
         }
         //Create Arraylist of class type
-        ArrayList<FootballTeam> footballList =new ArrayList<FootballTeam>();
-        ArrayList<BasketballTeam> basketballList =new ArrayList<BasketballTeam>();
+
         //Create instance
         FootballTeam football;
         BasketballTeam basketball;
@@ -70,8 +71,8 @@ public class GameResult {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        SortedList(footballList,basketballList);
-        getResult(footballList,basketballList);
+       // SortedList(footballList,basketballList);
+        //getResult(footballList,basketballList);
     }
 
     public void getResult(ArrayList<FootballTeam> t1, ArrayList<BasketballTeam> t2)
@@ -120,13 +121,59 @@ public class GameResult {
             }
 
         }
-        System.out.println("New Result after update point");
-        SortedList(t1,t2);
+      //  System.out.println("New Result after update point");
+       // SortedList(t1,t2);
     }
     public static void main(String[] args)
     {
         GameResult g=new GameResult();
-        g.createTeams();
+       // g.createTeams();
+        ArrayList<FootballTeam> footballList =new ArrayList<FootballTeam>();
+        ArrayList<BasketballTeam> basketballList =new ArrayList<BasketballTeam>();
+
+        int temp=1;
+      //  int ch;
+        while(temp!=0)
+        {
+        System.out.println("1.Load teams parameters from teams.csv file; for each line define the instance for a team; \n" +
+                "2.Print standing of Football teams & Basketball teams (ordering by points); \n" +
+                "3.Load results of the last matches from results.csv file and update teams points based on loaded results; \n" +
+                "4.Print new standing of Football teams and Basketball teams(ordering by points); \n" +
+                "5.Exit" );
+        System.out.println("Enter the choice");
+        Scanner sc=new Scanner(System.in);
+       int  ch=sc.nextInt();
+        switch(ch)
+            {
+                case 1:
+                    g.createTeams(footballList,basketballList);
+                    System.out.println("Parameters load in CSV Successfully");
+                    break;
+                case 2:
+                    System.out.println("Standing of Teams");
+                   g. SortedList(footballList,basketballList);
+                    break;
+                case 3:
+                    g.getResult(footballList,basketballList);
+                    System.out.println("Results of the last matches from results.csv file loaded successfully");
+                    break;
+
+                case 4:
+                    System.out.println("New Standing after updating points");
+                    g.SortedList(footballList,basketballList);
+
+                    break;
+
+
+                case 5:
+                    temp=0;
+                    break;
+                default:
+                    System.out.println("Select correct option");
+                    break;
+            }
+
+        }
 
 
     }
